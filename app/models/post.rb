962d21b2490eb1a109cfb8124d3eeffb47aad181
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-12 22:34:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,10 @@
 class Post < ActiveRecord::Base
   belongs_to :author
   
-  
+   def show
+    @post = Post.find(params[:id])
+    render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}])
+  end
 
 end
